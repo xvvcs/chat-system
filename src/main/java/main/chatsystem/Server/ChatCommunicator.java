a07220d0815ac1e71eb7c7cbb35fce3c6c70cb8a
Richardzik <kubrama04@gmail.com>
2024-03-28 16:32:59 +0100

    Logging / server

--- src/main/java/main/chatsystem/Server/ChatCommunicator.java
+++ src/main/java/main/chatsystem/Server/ChatCommunicator.java
@@ -1,7 +1,12 @@
 package main.chatsystem.Server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import main.chatsystem.Model.User;
 
 public class ChatCommunicator implements Runnable {
     private final Socket socket;
@@ -16,6 +21,52 @@ public class ChatCommunicator implements Runnable {
 
     @Override
     public void run() {
+        Gson gson = new Gson();
+        try {
+            while (true) {
+                BufferedReader reader = StreamsFactory.createReader(socket);
+                PrintWriter writer = StreamsFactory.createWriter(socket);
+                String method = reader.readLine();
+                if (!method.equals("connect")) {
+                    writer.println("Disconnected");
+                    writer.flush();
+                    break;
+                }
+                writer.println("login required");
+                writer.flush();
+                String loginData = reader.readLine();
+                try{
+                    User login = gson.fromJson(loginData,User.class);
+                    if(!login.getNickname().isEmpty() || !login.getPassword().isEmpty()) {
+                        writer.println("Approved");
+                        System.out.println("Logged successfully");
+                        writer.flush();
+                    }
+                    else
+                    {
+                        writer.println("Disapproved");
+                        System.out.println("Logging procedure failure");
+                        writer.flush();
+                    }
+                }
+                catch (JsonSyntaxException e)
+                {
+                    writer.println("Disconnected");
+                    writer.flush();
+                    e.printStackTrace();
+                }
 
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
