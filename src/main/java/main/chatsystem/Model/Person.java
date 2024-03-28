d4896e2df8a1b11fa224b0b96f3c6a664473a55b
ass97 <maciek.m2004@wp.pl>
2024-03-28 14:39:50 +0100

    Update User.java

--- src/main/java/main/chatsystem/Model/Person.java
+++
@@ -1,9 +0,0 @@
-package main.chatsystem.Model;
-
-public class Person {
-    private final nickname;
-
-    public Person(String nickname) {
-        this.nickname = nickname;
-    }
-}
---
+++ src/main/java/main/chatsystem/Model/User.java
@@ -0,0 +1,41 @@
+package main.chatsystem.Model;
+
+import java.util.Objects;
+
+public class User {
+    private final String nickname;
+    private final String password;
+
+    public User(String nickname, String password) {
+        this.nickname = nickname;
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "nickname='" + nickname + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(nickname, user.nickname) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nickname, password);
+    }
+}
