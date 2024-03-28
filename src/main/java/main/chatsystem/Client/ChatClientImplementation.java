f2ffd485bd78a572cc3bcd6ffaddf92e9bf5146d
ass97 <maciek.m2004@wp.pl>
2024-03-28 15:20:12 +0100

    Update ChatClientImplementation.java

--- src/main/java/main/chatsystem/Client/ChatClientImplementation.java
+++ src/main/java/main/chatsystem/Client/ChatClientImplementation.java
@@ -1,8 +1,11 @@
 package main.chatsystem.Client;
 
 import com.google.gson.Gson;
+import main.chatsystem.Model.User;
 import main.chatsystem.Server.StreamsFactory;
 
+import javax.xml.transform.Result;
+import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,9 +26,45 @@ public class ChatClientImplementation implements ChatClient {
         gson = new Gson();
         support = new PropertyChangeSupport(this);
         
-        // listener = new MessageListener(this, "230.0.0.0" , 888);
-        // Thread thead = new Thread(listener);
-        // thead.start();
+         listener = new MessageListener(this, "230.0.0.0" , 888);
+         Thread thead = new Thread(listener);
+         thead.start();
     }
 
+    @Override
+    public void disconnect() throws IOException {
+        writer.println("Disconnect");
+        writer.flush();
+        String reply = reader.readLine();
+        if (!reply.equals("Disconnected")) {
+            throw new IOException("Protocol failure");
+        }
+        socket.close();
+    }
+
+    @Override
+    public void login(String username, String password) {
+
+    }
+
+    @Override
+    public void sendMessage(String message, User user) {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        support.removePropertyChangeListener(listener);
+    }
+
+    @Override
+    public void receiveBroadcast(Object object) {
+        Result result = gson.fromJson(object.toString(), Result.class);
+        support.firePropertyChange("result", null, result);
+    }
 }
