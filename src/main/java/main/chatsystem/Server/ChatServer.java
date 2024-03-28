1af829e80451f2bdd92f1d1faa994ffc770db451
Richardzik <kubrama04@gmail.com>
2024-03-28 14:07:33 +0100

    ChatServer class

---
+++ src/main/java/main/chatsystem/Server/ChatServer.java
@@ -0,0 +1,28 @@
+package main.chatsystem.Server;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+    public static void main(String args[]) throws IOException
+    {
+        final int PORT = 8080;
+        System.out.println("Starting Server...");
+        System.out.println("Server IP: " + InetAddress.getLocalHost().getHostAddress());
+
+        // Creating server socket
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        UDPBroadcaster broadcaster = new UDPBroadcaster("230.0.0.0",8888);
+        while(true)
+        {
+            System.out.println("Waiting for a client...");
+            // Waiting for contacting with the client
+            Socket socket = serverSocket.accept();
+            ChatCommunicator communicator = new ChatCommunicator(socket);
+            Thread thread = new Thread(communicator);
+            thread.start();
+        }
+    }
+}
